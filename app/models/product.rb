f27ec0f4a3477251be620fb3604ce1d3acfac7cd
takashi <takashi@isanoMacBook-Air.local>
2022-04-08 08:44:15 +0900

    関連するpurchaseテーブル削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
     belongs_to :category
     belongs_to :sale_status
     belongs_to :product_status
-    has_many :purchases
+    belongs_to :user
+    has_many :purchases, dependent: :destroy
 end
