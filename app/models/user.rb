279e59e9238c87d9e880f3a745bdfa7f79ae9a58
takashi <takashi@isanoMacBook-Air.local>
2022-04-12 20:53:18 +0900

    #8 パス郵便・電話番号定数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_ZIPCODE_REGEX = /\A[0-9]+\z/
+  VALID_PHONE_NUMBER_REGEX = /\A[0-9]+\z/
 
   belongs_to :user_classification
   has_many :orders, dependent: :destroy
 
   validates :last_name, presence: true, length: { maximum: 10 }
   validates :first_name, presence: true, length: { maximum: 10 }
-  validates :password, presence: true, confirmation: true , format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, length: { in: 6..15 }
-  validates :zipcode, presence: true, format: { with: /\A[0-9]+\z/ }, length: { is: 7 }
+  validates :password, presence: true, confirmation: true , format: { with: VALID_PASSWORD_REGEX }, length: { in: 6..15 }
+  validates :zipcode, presence: true, format: { with: VALID_ZIPCODE_REGEX }, length: { is: 7 }
   validates :prefecture, presence: true, length: { maximum: 5 }
   validates :municipality, presence: true, length: { maximum: 10 }
   validates :address, presence: true, length: { maximum: 15 }
   validates :apartments, length: { maximum: 20 }
   validates :email, format: { with: VALID_EMAIL_REGEX }
-  validates :phone_number, presence: true, format: { with: /\A[0-9]+\z/ }, length: { minimum: 15 }
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }, length: { minimum: 15 }
 
 end
