412f9a1886aef6bfc457de2339d7d120f13b805c
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-04-22 08:43:16 +0900

    #18 インスタンス変数を修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
   def show
-    @products = Product.find_by(id: params[:id])
-    @categories = Category.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
   end
 end
