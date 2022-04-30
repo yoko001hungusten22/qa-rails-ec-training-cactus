1d62e2b70de97e3131ee2a12e957e84ceab0dc35
takashi <takashi@isanoMacBook-Air.local>
2022-05-01 00:03:15 +0900

    #ストロングパラメータ削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,4 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def user_params
-    params.require(:user).permit(:last_name, :first_name, :zipcode, :prefecture, :municipality, :address, :apartments, :email, :phone_number, :company_name, :password, :password_confirmation)
-  end
 end
