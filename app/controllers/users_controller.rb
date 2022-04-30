f01adeecde1f1aa64f4aaa2409a9b2ec29a3bab5
takashi <takashi@isanoMacBook-Air.local>
2022-04-30 10:31:01 +0900

    #22インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find_by(id: params[:id])
     if @user.update!(user_params)
-			flash[:success] = '更新に成功しました'
+      flash[:success] = '更新に成功しました'
       redirect_to @user
     else
       flash.now[:danger] = '更新に失敗しました'
