fe6728ca4366870f2d8067b5e84291c468e8bf24
takashi <takashi@isanoMacBook-Air.local>
2022-05-03 00:30:18 +0900

    #27商品検索画面(検索ロジック以外)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def index
+    @products = Product.all.page(params[:page]).per(10)
+  end
 end
