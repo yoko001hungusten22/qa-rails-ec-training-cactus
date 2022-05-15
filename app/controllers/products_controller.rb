7b096ef01de7fd3ade0d5615044d87fdcbf35ef9
takashi <takashi@isanoMacBook-Air.local>
2022-05-15 11:59:27 +0900

    #32_検索ロジック実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.page(params[:page]).per(10)
+  
+    if params[:product_name].present?
+      @products = @products.get_by_name params[:product_name]
+    end
+    if params[:category].present?
+      @products = @products.get_by_category params[:category]
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,16 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :purchases, dependent: :destroy
   has_many :order_details, dependent: :nullify
+
+  enum category: { 寝具: 1, 時計: 2, 食品: 3, 文具: 4 }
+
+  #商品名による絞り込み
+  scope :get_by_name, ->(product_name) {
+  where('product_name LIKE ?', "%#{product_name}%")
+  }
+  
+  #カテゴリーによる絞り込み
+  scope :get_by_category, ->(category) {
+  where(category: category)
+  }
 end
