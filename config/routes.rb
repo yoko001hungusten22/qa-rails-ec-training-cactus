0de847f13f00c80f575b18428e16015c5769bb0a
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-05-17 01:39:12 +0900

    #24 ログイン、ログアウト処理追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
