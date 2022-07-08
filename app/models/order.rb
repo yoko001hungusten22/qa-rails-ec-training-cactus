75634ef01a1cdfe0972fe0dee7e85ba116ff2372
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-07-09 01:32:48 +0900

    #20_yoko 注文詳細画面作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_details, dependent: :destroy
+
+  def find_preparation_shipment_status
+    order_details.any? {|order_detail| order_detail.shipment_status.shipment_status_name == '準備中'}
+  end
+
+  def all_products_total_price
+    order_details.sum {|order_detail| order_detail.product.price * order_detail.order_quantity}
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,8 @@ class OrderDetail < ApplicationRecord
   belongs_to :product
   belongs_to :order
   belongs_to :shipment_status
+
+  def one_product_total_price
+    product.price * order_quantity
+  end
 end
