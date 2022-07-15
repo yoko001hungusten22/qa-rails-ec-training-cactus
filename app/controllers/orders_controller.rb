6ca8c582449951cccc58f99b10f81e314cc093f4
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-07-16 01:21:36 +0900

    #33 注文履歴ページ作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
   end
+
+  def index
+    @orders = Order.order('id').page(params[:page]).per(10)
+  end
 end
