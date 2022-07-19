82135f892e169e636606dab8df94f06ba09415dc
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-07-20 01:09:04 +0900

    #34 注文キャンセル処理作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,9 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
   end
+
+  def destroy
+    @order = Order.find_by(id: params[:id]).destroy!
+    redirect_to order_path
+  end
 end
