67ef5edb69c559534932a9c518e24c67c0589a79
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-07-20 23:19:23 +0900

    #33 before_actionのlogged_in_userにindexを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :logged_in_user, only: [:show]
+  before_action :logged_in_user, only: [:show, :index]
   before_action :correct_user, only: [:show]
   include SessionsHelper
 
