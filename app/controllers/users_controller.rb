4144fa2d798817d623cf789048f616a7f36c245e
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-08-05 16:31:21 +0900

    #29 ユーザ登録ページ、登録処理作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,21 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。こちらからログインしてください。'
+      redirect_to login_path
+    else
+      flash.now[:danger] = '登録に失敗しました'
+      render "new"
+    end
+  end
+
   def update
     @user = User.find_by(id: params[:id])
     if @user.update!(user_params)
