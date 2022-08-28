b63f4ec15cd5735e063f87e88e05bc67c4ff8192
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-08-28 19:21:10 +0900

    #37 updateアクションにbefore_actionの設定を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  before_action :ensure_normal_user, only: [:edit, :destroy]
+  before_action :ensure_normal_user, only: [:edit, :update, :destroy]
 
   include SessionsHelper
   
