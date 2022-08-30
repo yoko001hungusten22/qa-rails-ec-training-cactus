0e4a08f0eada373a52f6547b2568d0248929240f
Ryo Yokoyama <hysteric.barbie91@gmail.com>
2022-08-30 18:37:23 +0900

    #37 guest_sign_inメソッドの定義を、static_pages_controller.rbからsessions_controller.rbに変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,4 +17,22 @@ class SessionsController < ApplicationController
     log_out
     redirect_to login_path
   end
+
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user.password =  SecureRandom.urlsafe_base64(6)
+    user.last_name =  "ゲスト"
+    user.first_name = "ユーザー"
+    user.zipcode = "1234567"
+    user.prefecture = "北海道"
+    user.municipality = "札幌市"
+    user.address = "1-23-4"
+    user.phone_number = "080123456789"
+    user.company_name = "株式会社ゲスト"
+    user.user_classification_id = 1
+    end
+    log_in(user)
+    flash[:success] = 'ゲストユーザーとしてログインしました。'
+    redirect_to root_path
+  end
 end
